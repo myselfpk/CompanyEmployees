19b941e2c57169fec5f3b64b297f65424e31e5c0
Pushpendra Singh <myselfpk@hotmail.com>
2024-11-29 12:24:04 +0530

    Add project files.

---
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Contracts;
+using LoggerService;
+using Microsoft.Extensions.Options;
+using Repository;
+
+namespace CompanyEmployees.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services) => services.AddCors(Options =>
+        {
+            Options.AddPolicy("CorsPolicy", builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+        });
+
+        public static void ConfigureIISIntegration(this IServiceCollection services) => services.Configure<IISOptions>(Options =>
+        {
+
+        });
+        public static void ConfigureLoggerService(this IServiceCollection services) => services.AddSingleton<ILoggerManager, LoggerManager>();
+        public static void ConfigureRepositoryManager(this IServiceCollection services) => services.AddScoped<IRepositoryManager, RepositoryManager>();
+    }
+}
---
+++ Repository/RepositoryBase.cs
@@ -0,0 +1,28 @@
+﻿using Contracts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public abstract class RepositoryBase<T> : IRepositoryBase<T> where T : class
+    {
+        protected RepositoryContext RepositoryContext;
+
+        public RepositoryBase(RepositoryContext repositoryContext) => RepositoryContext = repositoryContext;
+
+        public IQueryable<T> FindAll(bool trackChanges) => !trackChanges ? RepositoryContext.Set<T>().AsNoTracking() : RepositoryContext.Set<T>();
+
+        public IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression, bool trackChanges) => !trackChanges ? RepositoryContext.Set<T>().Where(expression).AsTracking() : RepositoryContext.Set<T>().Where(expression);
+
+        public void Create(T entry) => RepositoryContext.Set<T>().Add(entry);
+
+        public void Update(T entry) => RepositoryContext.Set<T>().Update(entry);
+
+        public void Delete(T entry) => RepositoryContext.Set<T>().Remove(entry);
+    }
+}
